b938b9906e781eb6e99084b8b4f8426c514ba553
Samad Khan <khan.samad47@gmail.com>
2017-05-01 20:32:54 -0400

    Adding model for posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+# This class is used to modify the admin display of the models
+# of this application
+class PostModelAdmin(admin.ModelAdmin):
+    # This controls what will be displayed at the row of each entry
+    list_display = ["title", "timestamp","updated"]
+    
+    # Which column is clickable
+    list_display_links = ["updated"]
+
+    # Adds filter to the columns
+    list_filter = ["updated", "timestamp"]
+
+    # Add searchability
+    search_fields = ["title","content"]
+
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostModelAdmin)
